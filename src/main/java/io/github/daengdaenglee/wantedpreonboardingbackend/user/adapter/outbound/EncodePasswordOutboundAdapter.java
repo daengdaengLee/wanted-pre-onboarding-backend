f7fa25bc9095c2c70b2a2b2b5b17cc69514e6d32
daengdaengLee <gunho1020@gmail.com>
2023-08-02 16:08:39 +0900

    refactor: 불필요한 빈 등록 제거

--- src/main/java/io/github/daengdaenglee/wantedpreonboardingbackend/user/adapter/outbound/EncodePasswordOutboundAdapter.java
+++ src/main/java/io/github/daengdaenglee/wantedpreonboardingbackend/user/adapter/outbound/EncodePasswordOutboundAdapter.java
@@ -1,17 +1,14 @@
 package io.github.daengdaenglee.wantedpreonboardingbackend.user.adapter.outbound;
 
 import io.github.daengdaenglee.wantedpreonboardingbackend.user.application.outbound.EncodePasswordOutboundPort;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EncodePasswordOutboundAdapter implements EncodePasswordOutboundPort {
 
-    private final PasswordEncoder passwordEncoder;
-
-    public EncodePasswordOutboundAdapter(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
-    }
+    private final PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
     @Override
     public String encode(String rawPassword) {
