a139359d57a54fab2ab251adf457fc4103020e02
daengdaengLee <gunho1020@gmail.com>
2023-08-03 11:35:07 +0900

    feat: 게시글 조회 API 엔드포인트에 인증 로직 추가

--- src/main/java/io/github/daengdaenglee/wantedpreonboardingbackend/post/adapter/inbound/PostController.java
+++ src/main/java/io/github/daengdaenglee/wantedpreonboardingbackend/post/adapter/inbound/PostController.java
@@ -83,6 +83,11 @@ public class PostController {
     public SinglePostOutputDto readPost(
             @PathVariable("postId") Long postId,
             Authentication authentication) {
+        var authResult = Auth.create(authentication);
+        if (authResult.isEmpty()) {
+            throw new SimpleApiException(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다.");
+        }
+
         throw new RuntimeException("not implemented");
     }
 
