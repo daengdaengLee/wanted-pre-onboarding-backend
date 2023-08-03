43858b3a214ecf3d66412485ea34f8cbe92f0745
daengdaengLee <gunho1020@gmail.com>
2023-08-03 11:24:52 +0900

    feat: CreatePostService 구현

--- src/main/java/io/github/daengdaenglee/wantedpreonboardingbackend/post/application/service/CreatePostService.java
+++ src/main/java/io/github/daengdaenglee/wantedpreonboardingbackend/post/application/service/CreatePostService.java
@@ -3,14 +3,31 @@ package io.github.daengdaenglee.wantedpreonboardingbackend.post.application.serv
 import io.github.daengdaenglee.wantedpreonboardingbackend.post.application.inbound.CreatePostDto;
 import io.github.daengdaenglee.wantedpreonboardingbackend.post.application.inbound.CreatePostInboundPort;
 import io.github.daengdaenglee.wantedpreonboardingbackend.post.application.inbound.PostDto;
+import io.github.daengdaenglee.wantedpreonboardingbackend.post.application.inbound.UserDto;
+import io.github.daengdaenglee.wantedpreonboardingbackend.post.application.outbound.PostRepository;
+import io.github.daengdaenglee.wantedpreonboardingbackend.post.domain.Post;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreatePostService implements CreatePostInboundPort {
 
+    private final PostRepository postRepository;
+
+    public CreatePostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
     @Override
-    public PostDto createPost(CreatePostDto post) {
-        throw new RuntimeException("not implemented");
+    public PostDto createPost(CreatePostDto createPostDto) {
+        var post = this.postRepository.save(new Post(
+                createPostDto.title(),
+                createPostDto.content(),
+                createPostDto.author().id()));
+        return new PostDto(
+                post.id(),
+                post.title(),
+                post.content(),
+                new UserDto(post.author().id()));
     }
 
 }
